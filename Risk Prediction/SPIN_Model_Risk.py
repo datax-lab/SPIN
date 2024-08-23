30bc8413d3f6927f6ab028b172d497a1c9da7a46
Euiseong-Ko <59459609+Euiseong-Ko@users.noreply.github.com>
2024-08-23 10:31:04 -0700

    Update SPIN_Model_Risk.py

--- Risk Prediction/SPIN_Model_Risk.py
+++ Risk Prediction/SPIN_Model_Risk.py
@@ -5,43 +5,57 @@ import torch.nn as nn
 from SPIN_Utils_Risk import fixed_s_mask
 
 class SPIN(nn.Module):
-    def __init__(self, in_Nodes, pathway_Nodes, hidden_Nodes, out_Nodes, pathway_idx, initializer = "he_normal", activation = "Relu", dropout_Rates = .0):
-        super(SPIN, self).__init__()        
-        if activation == "Sigmoid":
-            self.activation = nn.Sigmoid()
-        elif activation == "Tanh":
-            self.activation = nn.Tanh()
-        elif activation == "Relu":
-            self.activation = nn.ReLU()
-        
+    def __init__(self, net_hparams, pathway_idx):
+        super(SPIN, self).__init__()
         self.pathway_idx = pathway_idx
-        self.sigmoid = nn.Sigmoid()
-        self.tanh = nn.Tanh()
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(dropout_Rates)
         ###########################################################################
         ### gene layer --> pathway layer
         ###########################################################################
-        self.layer1_female = nn.Linear(in_Nodes, pathway_Nodes) ### for female
-        self.layer1_male = nn.Linear(in_Nodes, pathway_Nodes) ### for male
+        self.layer1_female = nn.Linear(net_hparams[0], net_hparams[1][0]) ### for female
+        self.layer1_male = nn.Linear(net_hparams[0], net_hparams[1][0]) ### for male
         ### initialize weight & bias
         self.init_parameters(initializer, self.layer1_female.weight.data, self.layer1_female.bias)
         self.init_parameters(initializer, self.layer1_male.weight.data, self.layer1_male.bias)
         ###########################################################################
         ### pathway layer --> hidden layer
         ###########################################################################
-        self.layer2 = nn.Linear(pathway_Nodes, hidden_Nodes)
+        self.layer2 = nn.Linear(net_hparams[1][0], net_hparams[1][1])
         self.init_parameters(initializer, self.layer2.weight.data, self.layer2.bias)
         ###########################################################################
         ### hidden layer --> Output layer
         ###########################################################################
-        self.layer3 = nn.Linear(hidden_Nodes, out_Nodes)
+        self.layer3 = nn.Linear(net_hparams[1][1], net_hparams[2])
         self.init_parameters(initializer, self.layer3.weight.data, self.layer3.bias)
         ###########################################################################
         ### batch normalization & initialization
         ###########################################################################
-        self.bn1 = nn.BatchNorm1d(pathway_Nodes, eps = 1e-3, momentum = 0.99)
-        self.bn2 = nn.BatchNorm1d(hidden_Nodes, eps = 1e-3, momentum = 0.99)
+        self.bn1 = nn.BatchNorm1d(net_hparams[1][0])
+        self.bn2 = nn.BatchNorm1d(net_hparams[1][1])
+        ###########################################################################
+        ### activation
+        ###########################################################################
+        if net_hparams[4] == "sigmoid":
+            self.layer1_activation = nn.Sigmoid()
+            self.layer2_activation = nn.Sigmoid()
+        elif net_hparams[4] == "tanh":
+            self.layer1_activation = nn.Tanh()
+            self.layer2_activation = nn.Tanh()
+        elif net_hparams[4] == "relu":
+            self.layer1_activation = nn.ReLU()
+            self.layer2_activation = nn.ReLU()
+        elif net_hparams[4] == "lkrelu":
+            self.layer1_activation = nn.LeakyReLU()
+            self.layer2_activation = nn.LeakyReLU()
+        
+        ### output activation for binary classification
+        self.output_activation = nn.Sigmoid()
+
+        ### dropout
+        if net_hparams[5] != 0.:
+            self.on_Dropout = True
+            self.dropout = nn.Dropout(net_hparams[5])
+        else:
+            self.on_Dropout = False
     
     def init_parameters(self, init, weight, bias = None):
         ### initialize weight
@@ -59,7 +73,7 @@ class SPIN(nn.Module):
             bound = 1 / math.sqrt(fan_in)
             nn.init.uniform_(bias, -bound, bound)
             
-    def forward(self, x):
+    def forward(self, x, y, w):
         '''
         Input: 
             x: gene expression data, where the last column is sex information (e.g., 0 - female, 1 - male).            
@@ -69,33 +83,54 @@ class SPIN(nn.Module):
         torch.cuda.empty_cache()
         ##################################################################################
         ### gene layer --> pathway layer
-        if (0 in x[:, -1]) and (1 in x[:, -1]):
-            ### force the connections between gene layer and pathway layer w.r.t. 'pathway_mask'
+        if (0 not in x[:, -1]) and (1 not in x[:, -1]):
+            raise ValueError("No Sex Information")
+        else:
+            x_layer1 = []
+            labels = []
+            weights = []
+            if 0 in x[:, -1]:
+                ### force the connections between gene layer and pathway layer w.r.t. 'pathway_mask'
+                self.layer1_female.weight.data = fixed_s_mask(self.layer1_female.weight.data, self.pathway_idx)
+                idx_female = (x[:, -1] == 0).nonzero(as_tuple=True)
+                x_female = self.layer1_activation(self.layer1_female(x[:, :-1][idx_female]))
+                x_layer1.append(x_female)
+                labels.append(y[idx_female])
+                weights.append(w[idx_female])
+            if 1 in x[:, -1]:
+                ### force the connections between gene layer and pathway layer w.r.t. 'pathway_mask'
+                self.layer1_male.weight.data = fixed_s_mask(self.layer1_male.weight.data, self.pathway_idx)
+                idx_male = (x[:, -1] == 0).nonzero(as_tuple=True)
+                x_male = self.layer1_activation(self.layer1_male(x[:, :-1][idx_male]))
+                x_layer1.append(x_male)
+                labels.append(y[idx_male])
+                weights.append(w[idx_male])
+            
             self.layer1_female.weight.data = fixed_s_mask(self.layer1_female.weight.data, self.pathway_idx)
             self.layer1_male.weight.data = fixed_s_mask(self.layer1_male.weight.data, self.pathway_idx)
             x_female = self.activation(self.layer1_female(x[:, :-1][x[:, -1] == 0]))
             x_male = self.activation(self.layer1_male(x[:, :-1][x[:, -1] == 1]))            
             x = self.bn1(torch.cat((x_female, x_male), 0))
-        elif 0 in x[:, -1]:
-            self.layer1_female.weight.data = fixed_s_mask(self.layer1_female.weight.data, self.pathway_idx)
-            x = self.bn1(self.activation(self.layer1_female(x[:, :-1][x[:, -1] == 0])))
-        elif 1 in x[:, -1]:
-            self.layer1_male.weight.data = fixed_s_mask(self.layer1_male.weight.data, self.pathway_idx)
-            x = self.bn1(self.activation(self.layer1_male(x[:, :-1][x[:, -1] == 1])))
-        else:
-            raise ValueError("No information on the task vector")
-        ### dropout pathway layer
-        if self.training:
+        ### concatenate all sex group into a layer
+        x = torch.cat(x_layer1, dim = 0)
+        labels = torch.cat(labels, dim = 0)
+        weights = torch.cat(weights, dim = 0)
+
+        try:
+            x = self.bn1(x)
+        except:
+            x = x
+        if self.training & self.on_Dropout:
             x = self.dropout(x)
-        ##################################################################################
-        ### pathway layer --> hidden layer
-        x = self.bn2(self.activation(self.layer2(x)))
-        ### dropout hidden layer
-        if self.training:
+        ### pathway --> hidden layer
+        x = self.layer2_activation(self.layer2(x))
+        try:
+            x = self.bn2(x)
+        except:
+            x = x
+        if self.training & self.on_Dropout:
             x = self.dropout(x)
-        ##################################################################################
-        ### hidden layer --> output layer
-        x = self.sigmoid(self.layer3(x))
-            
-        return x
-    
+        ### hidden --> output layer
+        x = self.output_activation(self.layer3(x))
+        
+        return x, labels, weights
